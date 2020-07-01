a1d028d736b7dbc68e811f66fe0e2430394f8794
Ruslan Steiger <steiger.ruslan@gmail.com>
2020-07-01 09:55:06 +0200

    🎨

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>Index Page!</h1>
-    <NuxtLink to="/kunst-musik-design-allgemein">Subcategory exists</NuxtLink>
+    <NuxtLink to="/kunst-musik-design-allgemein"
+      >✅ Subcategory exists</NuxtLink
+    >
     <br />
-    <NuxtLink to="/not-valid">Subcategory does not exists</NuxtLink>
+    <NuxtLink to="/not-valid">❌ Subcategory does not exists</NuxtLink>
   </div>
 </template>
